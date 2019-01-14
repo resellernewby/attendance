bd1404c3549719cda9510b507d9788c9d4c65719
resellernewby <resellernewby@gmail.com>
2019-01-14 07:01:14 +0700

    first commit - sampai penambahan tombol untuk input data absen

---
+++ app/Http/Controllers/AttendanceController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Attendance;
+use Illuminate\Http\Request;
+
+class AttendanceController extends Controller
+{
+    public function list()
+    {
+        return Attendance::with('user')->get();
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('attendance');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return $request->all();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Attendance  $attendance
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Attendance $attendance)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Attendance  $attendance
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Attendance $attendance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Attendance  $attendance
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Attendance $attendance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Attendance  $attendance
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Attendance $attendance)
+    {
+        //
+    }
+}
---
+++ resources/js/components/AttendanceListComponent.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">
+                        <div>
+                            <button @click.prevent='buat()'>Buat</button>
+                        </div>
+                    </div>
+
+                    <div class="card-body">                        
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Presence</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="list in lists" :key="list.id">
+                                <td>{{list.user.name}}</td>
+                                <td>{{list.presence}}</td>
+                                </tr>                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                lists : {}
+            }
+        },
+        methods:{
+            loadData(){
+                axios.get('/list').then( ( {data} ) => (this.lists = data));
+            },
+            buat(){
+                alert('tes');
+                axios.post('/attendance', {tanggal: '2019-01-14'});
+                loadData();
+            }
+        },
+        mounted() {
+            this.loadData();
+        }
+    }
+</script>
